58cbdd93939ceabca4078933c23aa8263908a130
Hoang Khanh Bang <buibeo1234@gmail.com>
2022-06-02 12:12:15 +0700

    upload footer.php

---
+++ ivymoda-web-main/pages/footer.php
@@ -0,0 +1,33 @@
+<footer>
+            <section class="footer-item">
+             
+                    <div>
+                        <h1>TẢI ỨNG DỤNG IVY MODA</h1>
+                    </div>
+                    <div class="footer-img">
+                        <img src="https://pubcdn.ivymoda.com/images/appstore.jpg" alt="">
+                        <img src="https://pubcdn.ivymoda.com/images/googleplay.jpg" alt="">
+                    </div>
+                    <div class ="footer-form"> 
+                        <h1>Nhận bản tin IVY moda</h1>
+                        <span><input class="txt1" type="text" placeholder="Nhập email của bạn..."><i style ="opacity: 0.3;"class="fas fa-long-arrow-alt-left"></i></span> 
+                    </div>
+                    <div class="footer-menu">
+                        <a href=""><img src ="https://pubcdn.ivymoda.com/images/dathongbao.png"></a>           
+                        <a style =" border-left: 1px solid black;"href="">Liên hệ</a>
+                        <a href="">Tuyển dụng</a>
+                        <a style =" border-right: 1px solid black;"href="">Giới thiệu</a>
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-youtube"></i></a>
+
+                    </div>
+                    <div class="footer-end">
+                        <p>Công ty Cổ phần Cụ Kim với số đăng ký kinh doanh: 0105777650</p>
+                        <p>Địa chỉ đăng ký: Tổ dân phố Tháp, P.Đại Mỗ, Q.Nam Từ Liêm TP.Hà Nội, Việt Nam - 0243 206 2222 </p>                   
+                        <p>Đặt hàng online: 0246 662 3434</p>
+                        <br>
+                        <p>©Ivymoda All rights reserved</p>
+                    </div>
+                
+            </section>
+    </footer>
